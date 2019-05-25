395fb1ed087953a8eb41cd33e5aa927ebd10fb4c
Julien B <julien@toshokan.fr>
2019-05-25 09:13:59 +0200

    Refactoring lambda function code

--- lambda-startstop/lambda-startstop/files/lambda_function.py
+++ lambda-startstop/lambda-startstop/files/lambda_function.py
@@ -1,23 +1,26 @@
+"""
+Very simple Lambda function to Stop/Start Instances depending on
+targets declared in the environment variables of this function.
+
+Updates:
+  - 01 Oct. 2018: Adding support of suspend/resume for AGSs
+
+"""
+
 import boto3
 import json
 import logging
 import os
-import sys
 
-"""
-Very simple Lambda function to Stop/Start Instances depending on 
-targets declared in the environment variables of this function.
 
-Updates: 
-  - 01 Oct. 2018: Adding support of suspend/resume for AGSs 
-
-"""
-
-logger = logging.getLogger()
-logger.setLevel(logging.INFO)
+LOGGER = logging.getLogger()
+LOGGER.setLevel(logging.INFO)
 
 
 class NoTargetFoundError(Exception):
+    """
+    Generic exception raised when no suitable instances found.
+    """
     pass
 
 
@@ -37,24 +40,25 @@ def get_elligible_instances(instances, desired_status: str) -> list:
     """
     elligible_instances_ids = []
     nonsuitable_states = ['terminated', 'pending', 'stopping', 'shutting-down', 'rebooting']
-    logger.info('Beginning security checks')
+
+    LOGGER.info('Beginning security checks')
     for instance in instances:
         if instance.state['Name'] in nonsuitable_states:
-            logger.warning(f'Skipping instance {instance.id}'
+            LOGGER.warning(f'Skipping instance {instance.id}'
                            f'(non suitable state: {instance.state["Name"]})')
             continue
         
         if instance.state['Name'] == desired_status:
-            logger.warning(f'Skipping instance {instance.id}'
+            LOGGER.warning(f'Skipping instance {instance.id}'
                            f'(already in state state: {instance.state["Name"]})')
             continue
         
         if 'ebs' not in instance.root_device_type:
-            logger.warning(f'Skipping instance {instance.id} because root'
+            LOGGER.warning(f'Skipping instance {instance.id} because root'
                            f' device is non-EBS one')
             continue
 
-        logger.info(f'{instance.id} is OK for traitment')
+        LOGGER.info(f'{instance.id} is OK for traitment')
         elligible_instances_ids.append(instance.id)
 
     if not elligible_instances_ids:
@@ -66,17 +70,17 @@ def get_elligible_instances(instances, desired_status: str) -> list:
 def build_instance_filters() -> list:
     """
     This function will return a list of dictionaries used to filter
-    instances based on lambda's environment varaibles
+    instances based on lambda's environment variables
     
     :returns: list of "AWS filters" dictionaries {'Name':'tag:<key>' Values: [<value>]}
-    :raises NoTargetFoundError: if no tag will be found in environment varaibles
+    :raises NoTargetFoundError: if no tag will be found in environment variables
     """
 
-    logger.info('Seeking for Target environment variables')
+    LOGGER.info('Seeking for Target environment variables')
     filters = []
     for key in os.environ:
         if key.startswith('Target'):
-            logger.info(f'Found {key} with value {os.environ[key]}')
+            LOGGER.info(f'Found {key} with value {os.environ[key]}')
             tag_name, tag_value = os.environ[key].split(':')
             filters.append({'Name': f'tag:{tag_name}', 'Values': [tag_value]})
 
@@ -86,62 +90,62 @@ def build_instance_filters() -> list:
     return filters
 
 
-def start_instances_and_resume_asg(InstanceIds: list):
+def start_instances_and_resume_asg(instance_ids: list):
     """
     Start the instances, wait the running state and then resume the ASGs if
     needed
-    :param InstanceIds: (list) A list of instance IDs
+    :param instance_ids: (list) A list of instance IDs
     """
     client = boto3.client('autoscaling')
     wait_for_running = boto3.client('ec2').get_waiter('instance_running')
     auto_scaling_groups = set()
     
     # First, we start the instances
-    logger.info(f'Start instances: {InstanceIds}')
-    boto3.client('ec2').start_instances(InstanceIds=InstanceIds)
+    LOGGER.info(f'Start instances: {instance_ids}')
+    boto3.client('ec2').start_instances(InstanceIds=instance_ids)
     
     # Then, we waiting for all the instances are in running state
-    logger.info('Waiting for instances reach running state')
-    wait_for_running.wait(InstanceIds=InstanceIds)
+    LOGGER.info('Waiting for instances reach running state')
+    wait_for_running.wait(InstanceIds=instance_ids)
     
     # We're looking for existing ASGs
-    logger.info('Looking for existing auto scaling groups')
-    asg = client.describe_auto_scaling_instances(InstanceIds=InstanceIds)
+    LOGGER.info('Looking for existing auto scaling groups')
+    asg = client.describe_auto_scaling_instances(InstanceIds=instance_ids)
     for instance in asg['AutoScalingInstances']:
         if instance['AutoScalingGroupName']:
-            logger.info(f"Found ASG {instance['AutoScalingGroupName']}")
+            LOGGER.info(f"Found ASG {instance['AutoScalingGroupName']}")
             auto_scaling_groups.add(instance['AutoScalingGroupName'])
 
     # Finally, if ASGs was found, we resume them 
     for asg in list(auto_scaling_groups):
-        logger.info(f'Resuming ASG {asg}')
+        LOGGER.info(f'Resuming ASG {asg}')
         client.resume_processes(AutoScalingGroupName=asg)
 
 
-def suspend_asg_and_stop_instances(InstanceIds: list):
+def suspend_asg_and_stop_instances(instance_ids: list):
     """
     Suspend ASG (if exists) and then stop EC2 instances
-    :param InstanceIds: (list) A list of instance IDs
+    :param instance_ids: (list) A list of instance IDs
     """
     client = boto3.client('autoscaling')
     auto_scaling_groups = set()
     
     # First, we are looking for all the related ASGs to the instance IDs
-    logger.info('Looking for existing auto scaling groups')
-    asg = client.describe_auto_scaling_instances(InstanceIds=InstanceIds)
+    LOGGER.info('Looking for existing auto scaling groups')
+    asg = client.describe_auto_scaling_instances(InstanceIds=instance_ids)
     for instance in asg['AutoScalingInstances']:
         if instance['AutoScalingGroupName']:
-            logger.info(f"Found ASG {instance['AutoScalingGroupName']}")
+            LOGGER.info(f"Found ASG {instance['AutoScalingGroupName']}")
             auto_scaling_groups.add(instance['AutoScalingGroupName'])
 
     # If ASGs was found, we suspend them to avoid instance termination
     for asg in list(auto_scaling_groups):
-        logger.info(f'Suspending ASG {asg}')
+        LOGGER.info(f'Suspending ASG {asg}')
         client.suspend_processes(AutoScalingGroupName=asg)
 
     # Finally, we can stop the instances
-    logger.info(f'Stop instances: {InstanceIds}')
-    boto3.client('ec2').stop_instances(InstanceIds=InstanceIds)
+    LOGGER.info(f'Stop instances: {instance_ids}')
+    boto3.client('ec2').stop_instances(InstanceIds=instance_ids)
 
 
 def main(desired_status: str, action_method):
@@ -157,25 +161,25 @@ def main(desired_status: str, action_method):
         filters = build_instance_filters()
 
         # Second, search for instances using that tags
-        logger.info('Let\'s see if any instance match with that tags')
+        LOGGER.info('Let\'s see if any instance match with that tags')
         instances = ec2.instances.filter(Filters=filters)
         if not len(list(instances)):
             raise NoTargetFoundError('No instances match with these tags. Exiting.')
         for instance in instances:
-            logger.info(f'Found instance {instance.id}')
+            LOGGER.info(f'Found instance {instance.id}')
 
         # Then, do some security checks (instance status and type of storage)
         target_instances = get_elligible_instances(instances, desired_status)
 
         # Finally let's do the job !
-        logger.info(f'Launch action on instances: {target_instances}')
+        LOGGER.info(f'Launch action on instances: {target_instances}')
         action_method(InstanceIds=target_instances)
         
-        return {'message' : 'Traitment done !'}
+        return {'message': 'Traitment done !'}
 
     except NoTargetFoundError as error:
-        logger.error(f'Stop Lambda because: {error}')
-        return json.dumps({'message': msg, 'code': 500})
+        LOGGER.error(f'Stop Lambda because: {error}')
+        return json.dumps({'message': f'{error}', 'code': 500})
 
 
 # Lambda Handlers (you can choose one to alter the behavior of this lambda)
@@ -184,15 +188,17 @@ def start_handler(event: dict, context: dict):
     """
     Start Handler use to only start instances
     """
-    logger.info('Beginning function : you want to START instances...')
+    LOGGER.info('Beginning function : you want to START instances...')
     main(desired_status='running', action_method=start_instances_and_resume_asg)
+
 
 def stop_handler(event: dict, context: dict):
     """
     Stop Handler use to only stop instances
     """
-    logger.info('Beginning function : you want to STOP instances...')
+    LOGGER.info('Beginning function : you want to STOP instances...')
     main(desired_status='stopped', action_method=suspend_asg_and_stop_instances)
+
 
 def start_or_stop_handler(event: dict, context: dict):
     """
